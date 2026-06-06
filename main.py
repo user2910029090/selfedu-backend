eedeba516ba2a08b817d9938aa2202a4521c70b1
otabek <abdusattorovotabek5@gmail.com>
2026-06-06 20:06:17 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.add_middleware(
 # Render'dan olingan kalitni o'qiydi
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 
 class AIChat(BaseModel):
     fan_nomi: str
